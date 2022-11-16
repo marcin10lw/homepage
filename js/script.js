e0f76374a9114c021e6b4e389acd1ccef0a82c8f
marcin10lw <marcin10lw@wp.pl>
2022-11-17 00:48:54 +0100

    updated styles for dark theme

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@
     };
 
     const toggleLinksTheme = () => {
-      const navigationLinks = document.querySelectorAll(".nav__link");
+      const navigationLinks = document.querySelectorAll(".js-link");
 
       for (link of navigationLinks) {
         link.classList.toggle("link--darkTheme");
@@ -29,10 +29,42 @@
       ballElement.classList.toggle("header__ball--darkTheme");
     };
 
+    const toggleTableHeadersTheme = () => {
+      const tableHeaderElements = document.querySelectorAll(".js-tableHeader");
+
+      for (header of tableHeaderElements) {
+        header.classList.toggle("table__header--darkTheme");
+      }
+    };
+
+    const toggleTableRowTheme = () => {
+      const tableRowElements = document.querySelectorAll(".js-tableRow");
+
+      for (row of tableRowElements) {
+        row.classList.toggle("table__row--darkTheme");
+      }
+    };
+
+    const toggleFooterTheme = () => {
+      const footerElement = document.querySelector(".js-footer");
+
+      footerElement.classList.toggle("footer--darkTheme");
+    };
+
+    const toggleMainTheme = () => {
+      const mainElement = document.querySelector(".js-main");
+
+      mainElement.classList.toggle("main--darkTheme");
+    };
+
     toggleBodyTheme();
     toggleLinksTheme();
     toggleLabelTheme();
     toggleBallTheme();
+    toggleTableHeadersTheme();
+    toggleTableRowTheme();
+    toggleFooterTheme();
+    toggleMainTheme();
   };
 
   const init = () => {
